6921c8d687575e402da1414f346b9fff36def9fb
markjso <97271827+markjso@users.noreply.github.com>
2022-09-18 12:19:30 +0930

    Create checks.c

---
+++ checks.c
@@ -0,0 +1,16 @@
+void	check_walls(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] != '1')
+		{
+			ft_putstr("map is not surrounded by walls", 2);
+			free(line);
+			exit (1);
+		}
+		i++;
+	}
+}
