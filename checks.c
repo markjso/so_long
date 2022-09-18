49a6132e7e09b6c4f2428c12b1a98edf2c94b058
markjso <97271827+markjso@users.noreply.github.com>
2022-09-18 12:31:30 +0930

    Update checks.c

--- checks.c
+++ checks.c
@@ -1,11 +1,11 @@
-void	check_walls(char *line)
+void	check_walls(t_map *map)
 {
 	int	i;
 
 	i = 0;
-	while (line[i] != '\0')
+	while (map->map[i] != '\0')
+	if (map->w[0][i] != '1' || [map->h - 1][i] != '1')
 	{
-		if (line[i] != '1')
 		{
 			ft_putstr("map is not surrounded by walls\n");
 			free(line);
