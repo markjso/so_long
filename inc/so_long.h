84f5e7fdf26f19fa89fed2c4450e7ee840b81bbe
markjso <97271827+markjso@users.noreply.github.com>
2022-10-01 19:34:14 +0930

    Add files via upload

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 10:30:37 by jmarks            #+#    #+#             */
-/*   Updated: 2022/09/28 16:16:27 by jmarks           ###   ########.fr       */
+/*   Updated: 2022/10/01 19:16:50 by jmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,9 @@ typedef struct s_map
 	int		steps;
 	int		end;
 	char	**map;
+	char	**map_copy;
+	int 	copy_ccount;
+	int		copy_ecount;
 	int		w;
 	int		h;
 	int		fd;
@@ -49,21 +52,22 @@ typedef struct s_map
 	void	*img;
 }	t_map;
 
-int	closeprogram(chaar *str);
-int	exitprogram(void);
+int		closeprogram(char *str);
+int		exitprogram(void);
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 void	ft_putnbr(int n);
 void	place_player(t_map *map, int height, int width);
 void	place_collectable(t_map *map);
 int		check_map_file(char *str);
-int		check_shape (t_map *map);
 void	check_walls(t_map *map);
-void	valid_map(t_map *map);
+int		valid_path(t_map *map, int y, int x);
+void	valid_count(t_map *map);
+int		valid_map(t_map *map);
 int		keypress_hook(int keycode, t_map *map);
 void	render_map(t_map *map);
 void	parse_map(t_map *map);
-void	map_check(t_map *map);
+void	copy_map(char **origin, char** copy);
 void	render_background(t_map *map);
 
 #endif
