cca29f9ba125636e74267f4acfabdf7146d63d68
markjso <97271827+markjso@users.noreply.github.com>
2022-10-01 20:04:05 +0930

    Add files via upload

--- sources/checks2.c
+++ sources/checks2.c
@@ -6,31 +6,38 @@
 /*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 19:02:08 by jmarks            #+#    #+#             */
-/*   Updated: 2022/10/01 19:32:42 by jmarks           ###   ########.fr       */
+/*   Updated: 2022/10/01 19:59:35 by jmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
-static void	flood_fill(t_map *map, int y, int x)
+static void	flood_fill(t_map *map, int paint, int y, int x)
 {
-	if (map->map_copy[y][x] == '1')
-		return ;
 	if (map->map_copy[y][x] == 'C')
+	{
 		map->copy_ccount++;
-	if (map->map_copy[y][x] == 'E')
+		map->map_copy[y][x] = '0';
+	}
+	else if (map->map_copy[y][x] == 'E')
+	{
 		map->copy_ecount++;
-	map->map_copy[y][x] = '1';
-	flood_fill(map, y, x + 1);
-	flood_fill(map, y, x - 1);
-	flood_fill(map, y + 1, x);
-	flood_fill(map, y - 1, x);
+		map->map_copy[y][x] = '0';
+	}
+	if (map->map_copy[y][x] == '0')
+	{
+		map->map_copy[y][x] = paint;
+		flood_fill(map, paint, y, x + 1);
+		flood_fill(map, paint, y, x - 1);
+		flood_fill(map, paint, y + 1, x);
+		flood_fill(map, paint, y - 1, x);
+	}
 }
 
 int	valid_path(t_map *map, int y, int x)
 {
 	map->map_copy = ft_calloc(1, sizeof(map->map));
 	copy_map(map->map, map->map_copy);
-	flood_fill(map, y, x);
+	flood_fill(map, 'P', y, x);
 	if (map->ccount != map->copy_ccount)
 	{
 		closeprogram("Map cannot be solved\n");
